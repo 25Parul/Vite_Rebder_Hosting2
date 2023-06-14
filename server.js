da208deee81d374ecf86d5a8d863ae18be21df23
paruljain-25 <107891240+paruljain-25@users.noreply.github.com>
2023-06-13 20:54:48 -0700

    Basic hosting on render using vite

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+const port = 3025;
+const app =express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "/dist")));
+
+app.listen(port, ()=>{
+    console.log("listening on port", port)
+});
---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>
+      The Future of Work: Embracing Automation and Remote Collaboration
+    </h1>
+    <p>
+      The future of work is being reshaped by automation and the rise of remote collaboration. As technologies like artificial intelligence, robotics, and machine learning continue to advance, certain tasks and jobs are becoming automated, enabling increased productivity and efficiency. Simultaneously, the growth of remote work and virtual collaboration tools allows teams to work together seamlessly regardless of geographical location. Embracing automation and remote collaboration offers opportunities for flexibility, improved work-life balance, and access to global talent pools. However, it also necessitates the development of new skills, adaptability, and effective management strategies to thrive in the evolving work landscape.
+    </p>
+
+    </>
+  )
+}
+
+export default App
